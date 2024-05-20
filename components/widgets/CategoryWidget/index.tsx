647ccc4d647899ed28a61f136c8a27995af36e73
adamrichardturner <hello@adamrichardturner.dev>
2024-05-20 08:20:59 +0100

    Category widget style changes

--- components/widgets/CategoryWidget/index.tsx
+++ components/widgets/CategoryWidget/index.tsx
@@ -6,55 +6,55 @@ function CategoryWidget({ active }: CategoryWidgetProps) {
   return (
     <ul className='w-full gap-x-2.5 gap-y-4 rounded-btn flex flex-row flex-wrap p-6 bg-white'>
       <li
-        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:bg-btn-upvote-background-hover hover:cursor-pointer ${
+        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:cursor-pointer ${
           active === "all"
             ? "bg-btn-upvote-active hover:bg-btn-upvote-active text-white"
-            : "bg-btn-upvote-background text-btn-upvote-active font-[500]"
+            : "bg-btn-upvote-background text-btn-upvote-active hover:bg-btn-upvote-background-hover font-[500]"
         }`}
       >
         All
       </li>
       <li
-        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:bg-btn-upvote-background-hover hover:cursor-pointer ${
+        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:cursor-pointer ${
           active === "ui"
             ? "bg-btn-upvote-active hover:bg-btn-upvote-active text-white"
-            : "bg-btn-upvote-background text-btn-upvote-active font-[500]"
+            : "bg-btn-upvote-background text-btn-upvote-active hover:bg-btn-upvote-background-hover font-[500]"
         }`}
       >
         UI
       </li>
       <li
-        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:bg-btn-upvote-background-hover hover:cursor-pointer ${
+        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:cursor-pointer ${
           active === "ux"
             ? "bg-btn-upvote-active hover:bg-btn-upvote-active text-white"
-            : "bg-btn-upvote-background text-btn-upvote-active font-[500]"
+            : "bg-btn-upvote-background text-btn-upvote-active hover:bg-btn-upvote-background-hover font-[500]"
         }`}
       >
         UX
       </li>
       <li
-        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:bg-btn-upvote-background-hover hover:cursor-pointer ${
+        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:cursor-pointer ${
           active === "enhancement"
             ? "bg-btn-upvote-active hover:bg-btn-upvote-active text-white"
-            : "bg-btn-upvote-background text-btn-upvote-active font-[500]"
+            : "bg-btn-upvote-background text-btn-upvote-active hover:bg-btn-upvote-background-hover font-[500]"
         }`}
       >
         Enhancement
       </li>
       <li
-        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:bg-btn-upvote-background-hover hover:cursor-pointer ${
+        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:cursor-pointer ${
           active === "bug"
             ? "bg-btn-upvote-active hover:bg-btn-upvote-active text-white"
-            : "bg-btn-upvote-background text-btn-upvote-active font-[500]"
+            : "bg-btn-upvote-background text-btn-upvote-active hover:bg-btn-upvote-background-hover font-[500]"
         }`}
       >
         Bug
       </li>
       <li
-        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:bg-btn-upvote-background-hover hover:cursor-pointer ${
+        className={`text-xs transition-colors py-1.5 px-4 rounded-btn hover:cursor-pointer ${
           active === "feature"
             ? "bg-btn-upvote-active hover:bg-btn-upvote-active text-white"
-            : "bg-btn-upvote-background text-btn-upvote-active font-[500]"
+            : "bg-btn-upvote-background text-btn-upvote-active hover:bg-btn-upvote-background-hover font-[500]"
         }`}
       >
         Feature
