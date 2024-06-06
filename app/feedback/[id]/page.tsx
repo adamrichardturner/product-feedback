43b38d899f27a7543b52575e7d12186b40174d3c
adamrichardturner <hello@adamrichardturner.dev>
2024-06-06 20:15:23 +0100

    Minor conditional render change

--- app/feedback/[id]/page.tsx
+++ app/feedback/[id]/page.tsx
@@ -58,13 +58,13 @@ export default function Page({ params }: { params: { id: string } }) {
           <BackButton isDark={true} />
         </div>
         <div>
-          {feedback?.user_id === user.id && (
+          {feedback?.user_id === user.id ? (
             <Link href={`/feedback/edit/${feedback?.id}`}>
               <div className='flex items-center rounded-btn py-2 px-4 space-x-1 text-white bg-[#4661E6] hover:bg-[#7C91F9] transition-colors cursor-pointer'>
                 <span className='font-semibold text-sm'>Edit Feedback</span>
               </div>
             </Link>
-          )}
+          ) : null}
         </div>
       </div>
       {loading ? (
