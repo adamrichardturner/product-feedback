1566258700c5fdeb6cdf713f37985630c69c4544
adamrichardturner <hello@adamrichardturner.dev>
2024-11-21 09:40:37 +0000

    Handle OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,53 @@ import "./globals.css"
 import { Toaster } from "@/components/ui/sonner"
 import { SWRProvider } from "@/components/providers/SWRProvider"
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000"
+const jost = Jost({ subsets: ["latin"] })
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
+  metadataBase: new URL(
+    process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000"
+  ),
   title: "Product Feedback App | Adam Richard Turner",
   description: "A full stack product feedback app with a Kanban board roadmap",
+  openGraph: {
+    title: "Product Feedback App | Adam Richard Turner",
+    description:
+      "A full stack product feedback app with a Kanban board roadmap",
+    url: "/",
+    siteName: "Product Feedback App",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Product Feedback App by Adam Richard Turner",
+      },
+    ],
+    locale: "en_GB",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Product Feedback App | Adam Richard Turner",
+    description:
+      "A full stack product feedback app with a Kanban board roadmap",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        alt: "Product Feedback App by Adam Richard Turner",
+      },
+    ],
+    creator: "@devadam88",
+  },
 }
-
-const jost = Jost({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
     <html lang='en' className={jost.className} suppressHydrationWarning>
       <body className='overflow-y-scroll bg-background text-foreground'>
